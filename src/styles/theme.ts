0e916cecc1c06f50bbb168179484aab7e1405404
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-06-09 14:22:55 +0900

    theme.tsの作成

---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+// テーマを定義するファイル(colorやfontsizeなど)
+
+const theme = {
+    // color : theme.colors.〇〇で使用可
+    colors: {
+        skyblue: "81C5EB",
+        green: "6CC4A1",
+        black: "000000",
+        white: "ffffff",
+        beige: "FFF3CF",
+    },
+
+    font: {
+        bold: "Rounded Mplus 1c Bold",
+    },
+
+    fontsize: {
+        xs: "16px",
+        sm: "20px",
+        base: "28px",
+        lg: "64px",
+        xl: "120px",
+    },
+
+    borderRadius: {
+        xs: "10px",
+        sm: "15px",
+        md: "30px",
+        lg: "40px",
+    },
+
+    border: {
+        bd: {
+            width: "4px",
+            style: "solid",
+        },
+    },
+};
+
+export default theme;
