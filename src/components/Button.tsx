00e1a9752f6c61b38c18a3fe21b7c81dbf52e2ae
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-06-16 14:07:58 +0900

    Buttonコンポーネントの作成

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,48 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+type ButtonProps = {
+    label: string;
+    to?: string; // 遷移先のパスを指定
+    onClick?: () => void;
+    className?: string;
+    disabled?: boolean;
+    // 進むボタンか戻るボタンか
+    direction: "forward" | "backward";
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    to,
+    onClick,
+    className = '',
+    direction,
+    disabled = false
+}) => {
+    const navigate = useNavigate();
+
+    const buttonClass = disabled
+        ? "bg-lightgray hover:bg-lightgray text-white opacity-50 cursor-not-allowed"
+        : direction === "forward"
+            ? "bg-blue hover:bg-blue text-white"
+            : "bg-gray hover:bg-lightgray text-white"
+
+    const handleClick = () => {
+        if (disabled) return;
+        if (onClick) onClick;
+        if (to) navigate(to);
+    }
+
+    return (
+        <button
+            onClick={handleClick}
+            disabled={disabled}
+            className={`w-[175px] h-[48px] text-sm rounded-sm border-xs border-white text-white focus:outline-none shadow-custom ${buttonClass} ${className}`}
+        >
+            {label}
+        </button>
+    );
+}
+
+export default Button;
+
