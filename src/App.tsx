98103ae1e9760dbe11a528fd08b483a9067df3b4
matsuokayuuki <matsu.yu-0114@ezweb.ne.jp>
2025-06-30 14:31:32 +0900

    ルーティングの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
+import ConnectPage from './pages/ConnectPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <StartPage />
+      <Routes>
+        <Route path='/' element={<StartPage />}></Route>
+        <Route path='/ConnectPage' element={<ConnectPage />}></Route>
+      </Routes>
+
     </BrowserRouter>
 
   )
